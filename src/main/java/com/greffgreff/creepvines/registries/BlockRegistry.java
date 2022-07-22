4fdabb67279a68045f5e0ce1c9203e81d5f74bc6
chandle greff <unconfigured@null.spigotmc.org>
2022-07-22 21:52:16 +0200

    Fixed method

--- src/main/java/com/greffgreff/creepvines/registries/BlockRegistry.java
+++ src/main/java/com/greffgreff/creepvines/registries/BlockRegistry.java
@@ -2,19 +2,24 @@ package com.greffgreff.creepvines.registries;
 
 import com.greffgreff.creepvines.Main;
 import com.greffgreff.creepvines.blocks.Creepvine;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<Block> CREEPVINE = registerBlock("creepvine", new Creepvine(), CreativeModeTab.TAB_MISC);
+    public static final RegistryObject<Block> CREEPVINE = registerBlock("creepvine", Creepvine::new);
 
-    public static RegistryObject<Block> registerBlock(String name, Block block, CreativeModeTab tab) {
-        ItemRegistry.registerBlockItem(name, block, tab);
-        return BLOCKS.register(name, () -> block);
+    public static <B extends Block> RegistryObject<B> registerBlock(String name, Supplier<B> supplier) {
+        RegistryObject<B> block = BLOCKS.register(name, supplier);
+        ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+        return block;
     }
 }
