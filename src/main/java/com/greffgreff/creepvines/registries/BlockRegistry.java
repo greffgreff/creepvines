190f2b042e2f0520b2db544c4a7c89e4b8774181
chandle greff <unconfigured@null.spigotmc.org>
2022-07-22 21:38:12 +0200

    Made blockitem register automatically

--- src/main/java/com/greffgreff/creepvines/registries/BlockRegistry.java
+++ src/main/java/com/greffgreff/creepvines/registries/BlockRegistry.java
@@ -2,6 +2,7 @@ package com.greffgreff.creepvines.registries;
 
 import com.greffgreff.creepvines.Main;
 import com.greffgreff.creepvines.blocks.Creepvine;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -10,9 +11,10 @@ import net.minecraftforge.registries.RegistryObject;
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
 
-    public static final RegistryObject<Block> CREEPVINE = registerBlock("creepvine", new Creepvine());
+    public static final RegistryObject<Block> CREEPVINE = registerBlock("creepvine", new Creepvine(), CreativeModeTab.TAB_MISC);
 
-    public static RegistryObject<Block> registerBlock(String name, Block block) {
+    public static RegistryObject<Block> registerBlock(String name, Block block, CreativeModeTab tab) {
+        ItemRegistry.registerBlockItem(name, block, tab);
         return BLOCKS.register(name, () -> block);
     }
 }
--- src/main/java/com/greffgreff/creepvines/registries/ItemRegistry.java
+++ src/main/java/com/greffgreff/creepvines/registries/ItemRegistry.java
@@ -5,14 +5,16 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.registries.RegistryObject;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ItemRegistry {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
-
-    public static final RegistryObject<Item> CREEPVINE_ITEM = registerBlockItem("creepvine", BlockRegistry.CREEPVINE.get(), CreativeModeTab.TAB_MISC);
 
     public static RegistryObject<Item> registerBlockItem(String name, Block block, CreativeModeTab tab) {
         return ITEMS.register(name, () -> new BlockItem(block, new Item.Properties().tab(tab)));
